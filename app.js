0686af240f2fc0cf0942310232dfb4ef177db20b
Vanessa Martin <vanessa@shopvida.com>
2019-04-20 20:58:47 -0700

    Prepared for deploy

--- app.js
+++ app.js
@@ -43,6 +43,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
